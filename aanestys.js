50be9f794a48dc1e98141728f926caecce98e9cd
Alsprite <henri.mykra@esedulainen.fi>
2022-09-12 12:48:33 +0300

    kurwa ibane

--- aanestys.js
+++ aanestys.js
@@ -1,4 +1,25 @@
 function kirjaudu() {
-    let balls = document.getElementById("nappula").value;
-    console.log(balls);
+    let ID = document.getElementById("ID").value;
+    console.log(ID);
+    let ss = document.getElementById("salasana").value;
+    console.log(ss);
+
+    if (ID == "admin" && ss == "1234") {
+        kirjauduAdmin();
+    } else {
+        kirjauduPerus();
+    }
+}
+function kirjauduAdmin() {
+    document.getElementById("kirjautuminen").style.display = "none";
+    document.getElementById("aanestysNappulat").style.display = "block";
+}
+function kirjauduPerus() {
+    document.getElementById("kirjautuminen").style.display = "none";
+}
+function uusiAanestys() {
+    document.getElementById("uusi").style.display = "block";
+}
+function sulje() {
+    document.getElementById("uusi").style.display = "none";
 }
